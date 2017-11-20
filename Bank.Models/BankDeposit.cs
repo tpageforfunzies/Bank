93b45c0948bd0d788be3f239d3229da67fbeaac4
tpageforfunzies <tjpage@indiana.edu>
2017-11-20 11:14:14 -0500

    added nav prop to deposit/withdrawal

--- Bank.Models/BankDeposit.cs
+++ Bank.Models/BankDeposit.cs
@@ -13,5 +13,7 @@ namespace Bank.Models
         public int TransactionID { get; set; }
 
         public decimal Amount { get; set; }
+
+        public virtual BankTransaction Transaction { get; set; }
     }
 }
