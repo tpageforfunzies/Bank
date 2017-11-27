55cd6c7be01dc3639f5fad0622c190a3b7f48652
Taylor Page <taylor.jay.page@gmail.com>
2017-11-27 11:19:11 -0500

    changed some styling

--- Bank.Web/App_Start/BundleConfig.cs
+++ Bank.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Bank.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Custom.css"));
         }
     }
 }
